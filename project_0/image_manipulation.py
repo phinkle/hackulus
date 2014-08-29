a5f2f82e222294d4292d7b3244f0327132f46a44
Rohan Ramchand <rohan@cs.utexas.edu>
2014-08-28 23:20:19 -0500

    finished project 0

--- project_0/image_manipulation.py
+++ project_0/image_manipulation.py
@@ -9,3 +9,36 @@ unit tests pass.
 """
 
 # TODO: Implement!
+
+import cv2
+import pep8
+import numpy
+
+
+def flip_image(img, horizontal, vertical):
+    flip_code = None
+
+    if horizontal and not vertical:
+        flip_code = 1
+
+    elif vertical and not horizontal:
+        flip_code = 0
+
+    elif horizontal and vertical:
+        flip_code = -1
+
+    else:
+        return img
+
+    return cv2.flip(img, flip_code)
+
+
+def negate_image(img):
+    out = []
+    for row in img:
+        out.append(cv2.absdiff(255, row))
+    return numpy.asarray(out)
+
+
+def swap_red_and_green(img):
+    return cv2.merge([cv2.split(img)[1], cv2.split(img)[0], cv2.split(img)[2]])
