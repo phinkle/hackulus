55fa7b9166fb890d42d76936952758bba507b506
Sai Avala <avala.sai@gmail.com>
2014-09-01 22:49:09 -0500

    passes all unit tests

--- project_0/image_manipulation.py
+++ project_0/image_manipulation.py
@@ -7,22 +7,27 @@ Use the unit tests is image_manipulation_test.py to guide
 your implementation, adding functions as needed until all
 unit tests pass.
 """
-
+import cv2
+import numpy as np
 # TODO: Implement!
+
 
 def flip_image(imageFile, horizontal, vertical):
     if (horizontal and vertical):
         return cv2.flip(imageFile, -1)
-    elif(horizontal):
-        return cv2.flip(imageFile, 0)
     elif(vertical):
-        return cv2.flip(imageFIle, 1)
+        return cv2.flip(imageFile, 0)
+    elif(horizontal):
+        return cv2.flip(imageFile, 1)
     else:
         return imageFile
 
+
 def negate_image(imageFile):
-    return cv2.subtract(np.full(nyc_image.shape, 255, dtype=np.uint8), imageFile)
+    return cv2.subtract(np.full(imageFile.shape, 255, dtype=np.uint8),
+                        imageFile)
+
 
 def swap_blue_and_green(imageFile):
-    b,g,r = cv2.split(imageFile)
-    return cv2.merge(g,b,r)
+    b, g, r = cv2.split(imageFile)
+    return cv2.merge([g, b, r])
