be93b722e99602074211cdfed31b795bcbe083c8
Conner Collins <ConnerCollins@utexas.edu>
2014-09-23 20:39:01 -0500

    tower panos added

--- project_1/mosaic.py
+++ project_1/mosaic.py
@@ -3,33 +3,34 @@ import cv2
 import numpy as np
 import pano_stitcher
 
-
-books_1 = cv2.imread("test_data/books_1.png")
-books_2 = cv2.imread("test_data/books_2.png")
-books_3 = cv2.imread("test_data/books_3.png")
+image_1 = cv2.imread("my_pano_pics/gdc_1.png")
+image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2BGRA)
+image_2 = cv2.imread("my_pano_pics/gdc_2.png")
+image_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2BGRA)
+image_3 = cv2.imread("my_pano_pics/gdc_3.png")
+image_3 = cv2.cvtColor(image_3, cv2.COLOR_BGR2BGRA)
 
 images = []
 origins = []
 
-homography = pano_stitcher.homography(books_2, books_1)
-books_1_warped, origin = pano_stitcher.warp_image(books_1, homography)
-images.append(books_1_warped)
+homography = pano_stitcher.homography(image_2, image_1)
+image_1_warped, origin = pano_stitcher.warp_image(image_1, homography)
+images.append(image_1_warped)
 origins.append(origin)
 
-# books_2 = cv2.cvtColor(books_2, cv2.COLOR_BGR2BGRA)
-# images.append(books_2)
-# origins.append((0, 0))
+images.append(image_2)
+origins.append((0, 0))
 
-# homography = pano_stitcher.homography(books_2, books_3)
-# print homography
-# books_3_warped, origin = pano_stitcher.warp_image(books_3, homography)
-# images.append(books_3_warped)
-# origins.append(origin)
+homography = pano_stitcher.homography(image_2, image_3)
+print homography
+image_3_warped, origin = pano_stitcher.warp_image(image_3, homography)
+images.append(image_3_warped)
+origins.append(origin)
 
-# print origins
+print origins
 
-# panorama = pano_stitcher.create_mosaic(images, origins)
+panorama = pano_stitcher.create_mosaic(images, origins)
 
-# cv2.imwrite("pano.png", panorama)
-cv2.imshow("books_1_warped", books_1_warped)
-cv2.waitKey(0)
+cv2.imwrite("gdc_pano.png", panorama)
+# cv2.imshow("panorama", panorama)
+# cv2.waitKey(0)
