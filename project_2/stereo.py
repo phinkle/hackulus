86e73dc4681a16d94cbd8452595e89f4244f0e87
Sai Avala <avala.sai@gmail.com>
2014-10-11 00:07:15 -0500

    added proj2

--- project_2/stereo.py
+++ project_2/stereo.py
@@ -5,7 +5,7 @@ In this project, you'll extract dense 3D information from stereo image pairs.
 
 import cv2
 import math
-import numpy
+import numpy as np
 
 
 def rectify_pair(image_left, image_right, viz=False):
@@ -19,7 +19,42 @@ def rectify_pair(image_left, image_right, viz=False):
       H_left, H_right: homographies that warp the left and right image so
         their epipolar lines are corresponding rows.
     """
-    pass
+
+    # Initiate SIFT detector
+    sift = cv2.SIFT()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(image_left, None)
+    kp2, des2 = sift.detectAndCompute(image_right, None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # store all the good matches as per Lowe's ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good])
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good])
+
+    F, mask = cv2.findFundamentalMat(src_pts, dst_pts, cv2.LMEDS)
+    src_pts = src_pts.flatten()
+    dst_pts = dst_pts.flatten()
+
+    retval, H_left, H_right = cv2.stereoRectifyUncalibrated(
+        src_pts, dst_pts, F, image_left.shape[:2])
+
+    print("HLEFT")
+    print(H_left)
+
+    return F, H_left, H_right
 
 
 def disparity_map(image_left, image_right):
@@ -32,7 +67,25 @@ def disparity_map(image_left, image_right):
       an single-channel image containing disparities in pixels,
         with respect to image_left's input pixels.
     """
-    pass
+
+    window_size = 3
+    min_disp = 0
+    num_disp = 112-min_disp
+    stereo = cv2.StereoSGBM(minDisparity=min_disp,
+        numDisparities=num_disp,
+        SADWindowSize=window_size,
+        uniquenessRatio=5,
+        speckleWindowSize=50,
+        speckleRange=160,
+        disp12MaxDiff=1,
+        P1=8*3*window_size**2,
+        P2=32*3*window_size**2,
+        fullDP=False
+    )
+
+    disp = stereo.compute(image_left, image_right).astype(np.uint8)
+
+    return disp
 
 
 def point_cloud(disparity_image, image_left, focal_length):
