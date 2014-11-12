9bf53035559f43773aa53546ce1450eb68993bf2
Paige Hinkle <paigehinkle@utexas.edu>
2014-11-11 18:05:06 -0600

    loaded male02

--- cardboard/js/main.js
+++ cardboard/js/main.js
@@ -77,6 +77,57 @@ function init() {
   mesh.rotation.x = -Math.PI / 2;
   scene.add(mesh);
 
+// texture
+
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+
+      console.log( item, loaded, total );
+
+    };
+
+    var texture = new THREE.Texture();
+
+    var onProgress = function ( xhr ) {
+      if ( xhr.lengthComputable ) {
+        var percentComplete = xhr.loaded / xhr.total * 100;
+        console.log( Math.round(percentComplete, 2) + '% downloaded' );
+      }
+    };
+
+    var onError = function ( xhr ) {
+    };
+
+
+    var loader = new THREE.ImageLoader( manager );
+    loader.load( 'textures/UV_Grid_Sm.jpg', function ( image ) {
+
+      texture.image = image;
+      texture.needsUpdate = true;
+
+    } );
+
+    // model
+
+    var loader = new THREE.OBJLoader( manager );
+    loader.load( 'obj/male02/male02.obj', function ( object ) {
+
+      object.traverse( function ( child ) {
+
+        if ( child instanceof THREE.Mesh ) {
+
+          child.material.map = texture;
+
+        }
+
+      } );
+
+      object.position.y = 0;
+      object.position.x = 100;
+      scene.add( object );
+
+    }, onProgress, onError );
+
   window.addEventListener('resize', resize, false);
   setTimeout(resize, 1);
 }
